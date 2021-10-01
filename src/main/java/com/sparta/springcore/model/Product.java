7862937f8be2d94fd8ea6dd18fd57c6a8e1741d0
salmon2 <dys04076@naver.com>
2021-10-01 10:12:12 +0900

    Unit Test Update

--- src/main/java/com/sparta/springcore/model/Product.java
+++ src/main/java/com/sparta/springcore/model/Product.java
@@ -4,8 +4,9 @@ import com.sparta.springcore.model.dto.ProductRequestDto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 import javax.persistence.*;
+
+import static com.sparta.springcore.validator.ProductValidator.ValidateProductInput;
 
 @Setter
 @Getter // get 함수를 일괄적으로 만들어줍니다.
@@ -39,7 +40,10 @@ public class Product {
 
     // 관심 상품 생성 시 이용합니다.
     public Product(ProductRequestDto requestDto, Long userId) {
-// 관심상품을 등록한 회원 Id 저장
+        // 입력값 Validation
+        ValidateProductInput(requestDto, userId);
+
+        // 관심상품을 등록한 회원 Id 저장
         this.userId = userId;
         this.title = requestDto.getTitle();
         this.image = requestDto.getImage();
@@ -47,4 +51,6 @@ public class Product {
         this.lprice = requestDto.getLprice();
         this.myprice = 0;
     }
+
+
 }
--- src/main/java/com/sparta/springcore/model/dto/ProductRequestDto.java
+++ src/main/java/com/sparta/springcore/model/dto/ProductRequestDto.java
@@ -1,14 +1,16 @@
 package com.sparta.springcore.model.dto;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class ProductRequestDto {
 
     private String title;
-    private String link;
     private String image;
+    private String link;
     private int lprice;
 
 }

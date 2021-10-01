2f29a23998879db41e743dbbece865723af40653
salmon2 <dys04076@naver.com>
2021-10-01 11:14:03 +0900

    integration Test Update

--- src/main/java/com/sparta/springcore/model/ProductMypriceRequestDto.java
+++ src/main/java/com/sparta/springcore/model/ProductMypriceRequestDto.java
@@ -1,8 +1,10 @@
 package com.sparta.springcore.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class ProductMypriceRequestDto {
     private int myprice;
 
